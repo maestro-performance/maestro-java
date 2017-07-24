21d70412f2102acd34ecb1e9835f146fe1d852d1
Otavio Rodolfo Piske <opiske@redhat.com>
2017-07-24 19:19:38 +0200

    NEW: added ping request groovy script

---
+++ src/main/groovy/Ping.groovy
@@ -0,0 +1,37 @@
+/*
+ *  Copyright ${YEAR} ${USER}
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import net.orpiske.mpt.main.Maestro
+
+@GrabConfig(systemClassLoader=true)
+
+@Grab(group='commons-cli', module='commons-cli', version='1.3.1')
+@Grab(group='org.apache.commons', module='commons-lang3', version='3.6')
+
+@Grab(group='org.msgpack', module='msgpack-core', version='0.8.3')
+
+@GrabResolver(name='Eclipse', root='https://repo.eclipse.org/content/repositories/paho-releases/')
+@Grab(group='org.eclipse.paho', module='org.eclipse.paho.client.mqttv3', version='1.1.1')
+
+brokerURL = System.getenv("MAESTRO_BROKER")
+
+println "Connecting to " + brokerURL
+maestro = new Maestro(brokerURL)
+
+maestro.pingRequest()
+maestro.stop()
+
+
