26c44195344270279e90a4d941d35e0974061c1a
Otavio Rodolfo Piske <opiske@redhat.com>
2017-10-12 12:39:18 +0200

    NEW: introduce a common abstract class for reusable test execution logic

---
+++ src/main/java/net/orpiske/mpt/test/AbstractTestExecutor.java
@@ -0,0 +1,80 @@
+package net.orpiske.mpt.test;
+
+import net.orpiske.mpt.maestro.Maestro;
+import net.orpiske.mpt.maestro.notes.MaestroNote;
+import net.orpiske.mpt.maestro.notes.PingResponse;
+import net.orpiske.mpt.reports.ReportsDownloader;
+import net.orpiske.mpt.utils.DurationParseException;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * A simple test executor that should be extensible for most usages
+ */
+public abstract class AbstractTestExecutor implements TestExecutor {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractTestExecutor.class);
+
+    private Maestro maestro;
+    private ReportsDownloader reportsDownloader;
+
+
+    public AbstractTestExecutor(final Maestro maestro, final ReportsDownloader reportsDownloader)
+            throws DurationParseException
+    {
+        this.maestro = maestro;
+        this.reportsDownloader = reportsDownloader;
+
+        logger.trace("Creating an abstract test executor");
+    }
+
+    protected Maestro getMaestro() {
+        return maestro;
+    }
+
+    protected ReportsDownloader getReportsDownloader() {
+        return reportsDownloader;
+    }
+
+    /**
+     * Start connected peers
+     * @throws MqttException
+     * @throws IOException
+     */
+    protected void startServices() throws MqttException, IOException {
+        maestro.startReceiver();
+        maestro.startInspector();
+        maestro.startSender();
+    }
+
+    /**
+     * Try to guess the number of connected peers
+     * @return the number of connected peers (best guess)
+     * @throws MqttException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    protected int getNumPeers() throws MqttException, IOException, InterruptedException {
+        int numPeers = 0;
+
+        logger.debug("Collecting responses to ensure topic is clean prior to pinging nodes");
+        maestro.collect();
+
+        logger.debug("Sending ping request");
+        maestro.pingRequest();
+
+        Thread.sleep(5000);
+
+        List<MaestroNote> replies = maestro.collect();
+        for (MaestroNote note : replies) {
+            if (note instanceof PingResponse) {
+                numPeers++;
+            }
+        }
+
+        return numPeers;
+    }
+}
--- src/main/java/net/orpiske/mpt/test/IncrementalTestExecutor.java
+++ src/main/java/net/orpiske/mpt/test/IncrementalTestExecutor.java
@@ -18,31 +18,25 @@ package net.orpiske.mpt.test;
 
 import net.orpiske.mpt.maestro.Maestro;
 import net.orpiske.mpt.maestro.notes.MaestroNote;
-import net.orpiske.mpt.maestro.notes.PingResponse;
 import net.orpiske.mpt.reports.ReportsDownloader;
 import net.orpiske.mpt.utils.DurationParseException;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.List;
 
-public class IncrementalTestExecutor implements TestExecutor {
+public class IncrementalTestExecutor extends AbstractTestExecutor {
     private static final Logger logger = LoggerFactory.getLogger(IncrementalTestExecutor.class);
 
-    private Maestro maestro;
     private IncrementalTestProfile testProfile;
 
     private long replyRetries;
-
-    private ReportsDownloader reportsDownloader;
     private IncrementalTestProcessor testProcessor;
 
     public IncrementalTestExecutor(final Maestro maestro, final ReportsDownloader reportsDownloader,
                                    final IncrementalTestProfile testProfile) throws DurationParseException {
-        this.maestro = maestro;
-        this.reportsDownloader = reportsDownloader;
+        super(maestro, reportsDownloader);
+
         this.testProfile = testProfile;
         this.testProcessor = new IncrementalTestProcessor(testProfile, reportsDownloader);
 
@@ -54,7 +48,7 @@ public class IncrementalTestExecutor implements TestExecutor {
         long repeat = (replyRetries * 2);
 
         while (testProcessor.getNotifications() != numPeers) {
-            List<MaestroNote> replies = maestro.collect(1000, 1);
+            List<MaestroNote> replies = getMaestro().collect(1000, 1);
 
             testProcessor.process(replies);
             repeat--;
@@ -66,47 +60,20 @@ public class IncrementalTestExecutor implements TestExecutor {
         }
     }
 
-    private int getNumPeers() throws MqttException, IOException, InterruptedException {
-        int numPeers = 0;
-
-        logger.debug("Collecting responses to ensure topic is clean prior to pinging nodes");
-        maestro.collect();
-
-        logger.debug("Sending ping request");
-        maestro.pingRequest();
-
-        Thread.sleep(5000);
-
-        List<MaestroNote> replies = maestro.collect();
-        for (MaestroNote note : replies) {
-            if (note instanceof PingResponse) {
-                numPeers++;
-            }
-        }
-
-        return numPeers;
-    }
-
-
-    private void startServices() throws MqttException, IOException {
-        testProcessor.resetNotifications();
-
-        maestro.startReceiver();
-        maestro.startInspector();
-        maestro.startSender();
-    }
 
     public boolean run() {
         try {
             // Clean up the topic
-            maestro.collect();
+            getMaestro().collect();
 
             while (!testProcessor.isCompleted()) {
                 int numPeers = getNumPeers();
 
-                reportsDownloader.setTestNum(testProfile.getTestExecutionNumber());
+                getReportsDownloader().setTestNum(testProfile.getTestExecutionNumber());
 
-                testProfile.apply(maestro);
+                testProfile.apply(getMaestro());
+                testProcessor.resetNotifications();
+
                 startServices();
                 processReplies(numPeers);
 
