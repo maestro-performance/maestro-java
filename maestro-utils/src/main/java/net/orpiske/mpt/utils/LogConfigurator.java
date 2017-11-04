0f34496833f5c4865a5d077c090a0b782b34a0ea
Otavio Rodolfo Piske <opiske@redhat.com>
2017-11-04 14:54:34 +0100

    FIX: incorrect package

--- maestro-utils/src/main/java/net/orpiske/mpt/utils/LogConfigurator.java
+++ maestro-utils/src/main/java/net/orpiske/mpt/utils/LogConfigurator.java
@@ -13,7 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-package utils;
+package net.orpiske.mpt.utils;
 
 import org.apache.log4j.PropertyConfigurator;
 
