dad6c1d17e9c6b3066e8aae31fba0448be3d8eb6
Otavio Rodolfo Piske <opiske@redhat.com>
2017-11-07 18:27:00 +0100

    UPD: initial ping implementation

--- maestro-worker/src/main/java/net/orpiske/mpt/maestro/worker/MaestroWorkerManager.java
+++ maestro-worker/src/main/java/net/orpiske/mpt/maestro/worker/MaestroWorkerManager.java
@@ -3,9 +3,17 @@ package net.orpiske.mpt.maestro.worker;
 import net.orpiske.mpt.common.exceptions.MaestroConnectionException;
 import net.orpiske.mpt.maestro.client.AbstractMaestroPeer;
 import net.orpiske.mpt.maestro.notes.MaestroNote;
+import net.orpiske.mpt.maestro.notes.PingRequest;
 import net.orpiske.mpt.maestro.notes.StartSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 
 public class MaestroWorkerManager extends AbstractMaestroPeer {
+    private static final Logger logger = LoggerFactory.getLogger(MaestroWorkerManager.class);
 
     public MaestroWorkerManager(final String url, final String clientName) throws MaestroConnectionException {
         super(url, clientName);
@@ -14,10 +22,24 @@ public class MaestroWorkerManager extends AbstractMaestroPeer {
 
     protected void messageArrived(MaestroNote note) {
 
+        if (note instanceof PingRequest) {
+            maestroMessageArrived((PingRequest) note);
+        }
     }
 
 
-    protected void messageArrived(StartSender note) {
+    protected void maestroMessageArrived(StartSender note) {
 
+    }
+
+    protected void maestroMessageArrived(PingRequest note) {
+        logger.debug("Creation seconds.micro: {}.{}", note.getSec(), note.getUsec());
+
+        Instant creation = Instant.ofEpochSecond(note.getSec(), note.getUsec() * 1000);
+        Instant now = Instant.now();
+
+        Duration d = Duration.between(creation, now);
+
+        logger.debug("Elapsed: {} ", d.getNano() / 1000);
     }
 }
