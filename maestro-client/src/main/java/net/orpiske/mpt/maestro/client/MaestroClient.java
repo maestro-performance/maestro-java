e8b4d821db93bde401b2789c2c79821c7ee5a717
Otavio Rodolfo Piske <opiske@redhat.com>
2017-11-09 18:34:58 +0100

    UPD: comments

--- maestro-client/src/main/java/net/orpiske/mpt/maestro/client/MaestroClient.java
+++ maestro-client/src/main/java/net/orpiske/mpt/maestro/client/MaestroClient.java
@@ -18,8 +18,8 @@ package net.orpiske.mpt.maestro.client;
 
 import net.orpiske.mpt.common.URLUtils;
 import net.orpiske.mpt.common.exceptions.MaestroConnectionException;
+import net.orpiske.mpt.common.exceptions.MaestroException;
 import net.orpiske.mpt.maestro.notes.MaestroNote;
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -30,12 +30,20 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.UUID;
 
+/**
+ * A generic Maestro client that can be used for publishing or receiving messages (usually for publishing)
+ */
 public class MaestroClient {
     private static final Logger logger = LoggerFactory.getLogger(MaestroClient.class);
 
     private MqttClient mqttClient;
 
-    public MaestroClient(final String url) throws MaestroConnectionException {
+    /**
+     * Constructor
+     * @param url Maestro broker URL
+     * @throws MaestroException if unable to create the client
+     */
+    public MaestroClient(final String url) throws MaestroException {
         String adjustedUrl = URLUtils.sanizeURL(url);
 
         UUID uuid = UUID.randomUUID();
@@ -46,11 +54,15 @@ public class MaestroClient {
             mqttClient = new MqttClient(adjustedUrl, "maestro-java-" + clientId, memoryPersistence);
         }
         catch (MqttException e) {
-            throw new MaestroConnectionException("Unable create a MQTT client instance : " + e.getMessage(),
+            throw new MaestroException("Unable create a MQTT client instance : " + e.getMessage(),
                     e);
         }
     }
 
+    /**
+     * Connect to the maestro broker
+     * @throws MaestroConnectionException if unable to connect to the broker
+     */
     public void connect() throws MaestroConnectionException {
         MqttConnectOptions connOpts = new MqttConnectOptions();
 
@@ -64,6 +76,11 @@ public class MaestroClient {
         }
     }
 
+
+    /**
+     * Disconnects from the maestro broker
+     * @throws MaestroConnectionException if failed to disconnect cleanly (should be safe to ignore in most cases)
+     */
     public void disconnect() throws MaestroConnectionException {
         try {
             mqttClient.disconnect();
@@ -73,6 +90,14 @@ public class MaestroClient {
         }
     }
 
+
+    /**
+     * Publishes a message in the broker
+     * @param topic the topic to publish the message
+     * @param note the maestro note to publish
+     * @throws MaestroConnectionException if failed to publish the message
+     * @throws IOException in case of other I/O errors
+     */
     public void publish(final String topic, final MaestroNote note) throws MaestroConnectionException, IOException {
         byte[] bytes = note.serialize();
 
