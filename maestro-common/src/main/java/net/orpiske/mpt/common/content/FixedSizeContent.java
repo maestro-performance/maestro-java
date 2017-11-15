f8531c89fec76a44eb7b9d05a33390bda84fbe74
Otavio Rodolfo Piske <opiske@redhat.com>
2017-11-15 12:59:22 +0100

    NEW: code to generate random content for the messages

---
+++ maestro-common/src/main/java/net/orpiske/mpt/common/content/FixedSizeContent.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2017 Otavio R. Piske <angusyoung@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.orpiske.mpt.common.content;
+
+/**
+ * A fixed-sized string message content
+ */
+public class FixedSizeContent implements ContentStrategy {
+    private static String content;
+    private StringBuffer buffer;
+
+
+    /*
+     * @see ContentStrategy#setSize(int)
+     */
+    @Override
+    public void setSize(int size) {
+        buffer = new StringBuffer();
+
+        buffer = new StringBuffer(size);
+
+        ContentFiller.randomFill(buffer, size);
+
+        content = buffer.toString();
+
+    }
+
+    /*
+     * @see ContentStrategy#getContent()
+     */
+    @Override
+    public String getContent() {
+        return content;
+    }
+}
---
+++ maestro-common/src/main/java/net/orpiske/mpt/common/content/VariableSizeContent.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2017 Otavio R. Piske <angusyoung@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.orpiske.mpt.common.content;
+
+/**
+ * A variable-size String message content
+ */
+public class VariableSizeContent implements ContentStrategy {
+    private StringBuffer buffer;
+    private int limit = 0;
+
+
+    /*
+     * @see ContentStrategy#setSize(int)
+     */
+    @Override
+    public void setSize(int size) {
+        int lowerBound = 0;
+        int upperBound = 0;
+
+        if (size >= 100) {
+            int bound = ((size / 100) * 5);
+
+            lowerBound = size - bound;
+            upperBound = size + bound;
+
+            limit = upperBound - lowerBound;
+        } else {
+            lowerBound = size - 1;
+            upperBound = size + 1;
+            limit = upperBound - lowerBound;
+        }
+
+        buffer = new StringBuffer(upperBound);
+
+        ContentFiller.randomFill(buffer, upperBound);
+    }
+
+
+    /*
+     * @see ContentStrategy#getContent()
+     */
+    @Override
+    public String getContent() {
+        int start = ((int) Math.random() % limit);
+
+        return buffer.substring(start);
+    }
+}
