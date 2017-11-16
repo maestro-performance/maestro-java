bb6989b2c24d3748a35d6a7db22c2e3fabd72c0a
Otavio Rodolfo Piske <opiske@redhat.com>
2017-11-16 14:12:30 +0100

    UPD: worker container documentation

--- maestro-worker/src/main/java/net/orpiske/mpt/maestro/worker/base/WorkerContainer.java
+++ maestro-worker/src/main/java/net/orpiske/mpt/maestro/worker/base/WorkerContainer.java
@@ -8,6 +8,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * This is a container class for multiple workers. It is responsible for
+ * creating, starting and stopping multiple workers at once.
+ */
 public final class WorkerContainer {
     private static WorkerContainer instance;
     private WorkerOptions workerOptions;
@@ -15,6 +19,10 @@ public final class WorkerContainer {
 
     private WorkerContainer() {}
 
+    /**
+     * Gets and instance of the container
+     * @return
+     */
     public synchronized static final WorkerContainer getInstance() {
         if (instance == null) {
             instance = new WorkerContainer();
@@ -24,15 +32,25 @@ public final class WorkerContainer {
     }
 
 
-    public WorkerOptions getWorkerOptions() {
-        return workerOptions;
-    }
-
+    /**
+     * Sets the worker options for the instance.
+     * @param workerOptions the worker options to set
+     */
     public void setWorkerOptions(WorkerOptions workerOptions) {
         this.workerOptions = workerOptions;
     }
 
-    public void start(final Class<MaestroWorker> clazz, BlockingQueue<WorkerSnapshot> snapshots) throws IllegalAccessException, InstantiationException {
+
+    /**
+     * Start the execution of the workers for a predefined class
+     * @param clazz The class associated with the workers
+     * @param snapshots The performance snapshot queue containing the performance test data
+     * @throws IllegalAccessException if unable to access the worker constructor
+     * @throws InstantiationException if unable to instantiate the worker
+     */
+    public void start(final Class<MaestroWorker> clazz, BlockingQueue<WorkerSnapshot> snapshots)
+            throws IllegalAccessException, InstantiationException
+    {
         int parallelCount = Integer.parseInt(workerOptions.getParallelCount());
 
         for (int i = 0; i < parallelCount; i++) {
