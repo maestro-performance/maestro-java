1893424b860c960e0e169199255ee65c760ba511
Otavio Rodolfo Piske <opiske@redhat.com>
2017-12-07 18:00:37 +0100

    REF: moves the log-related code to a separate class

--- maestro-worker/src/main/java/net/orpiske/mpt/maestro/worker/base/MaestroWorkerManager.java
+++ maestro-worker/src/main/java/net/orpiske/mpt/maestro/worker/base/MaestroWorkerManager.java
@@ -11,19 +11,14 @@ import net.orpiske.mpt.maestro.client.AbstractMaestroPeer;
 import net.orpiske.mpt.maestro.client.MaestroDeserializer;
 import net.orpiske.mpt.maestro.exceptions.MalformedNoteException;
 import net.orpiske.mpt.maestro.notes.*;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import static net.orpiske.mpt.maestro.worker.base.WorkerLogUtils.*;
 
 import static net.orpiske.mpt.maestro.worker.base.WorkerStateInfoUtil.isCleanExit;
 
@@ -70,34 +65,6 @@ public class MaestroWorkerManager extends AbstractMaestroPeer<MaestroEvent> impl
         return running;
     }
 
-    private File findLastLogDir() {
-        File currentLogDir = new File(logDir, "0");
-        File lastLogDir = currentLogDir;
-        int count = 0;
-
-        while (currentLogDir.exists()) {
-            lastLogDir = currentLogDir;
-
-            count++;
-            currentLogDir = new File(logDir, Integer.toString(count));
-        }
-
-        return lastLogDir;
-    }
-
-    private File findTestLogDir() {
-        File testLogDir = new File(logDir, "0");
-        int count = 0;
-
-        while (testLogDir.exists()) {
-            count++;
-            testLogDir = new File(logDir, Integer.toString(count));
-
-        }
-
-        testLogDir.mkdirs();
-        return testLogDir;
-    }
 
     @Override
     protected final void noteArrived(MaestroEvent note) throws IOException, MaestroConnectionException {
@@ -206,7 +173,7 @@ public class MaestroWorkerManager extends AbstractMaestroPeer<MaestroEvent> impl
             return false;
         }
 
-        final File testLogDir = findTestLogDir();
+        final File testLogDir = findTestLogDir(logDir);
 
         try {
             writeTestProperties(testLogDir);
@@ -323,54 +290,8 @@ public class MaestroWorkerManager extends AbstractMaestroPeer<MaestroEvent> impl
             }
         }
 
-        createSymlinks(failed);
+        createSymlinks(logDir, failed);
     }
-
-    private void deleteLinkQuietly(File lastLink) {
-        logger.debug("Deleting link {} after completing the test", lastLink.getName());
-
-        try {
-            FileUtils.deleteDirectory(lastLink);
-        } catch (IOException e) {
-            logger.warn("Unable to delete last link: " + e.getMessage());
-        }
-    }
-
-    private void createSymlinks(boolean failed) {
-        File lastLogDir = findLastLogDir();
-        Path target = Paths.get(lastLogDir.getAbsolutePath());
-
-        Path lastLink = Paths.get(lastLogDir.getParent() + File.separator + "last");
-        deleteLinkQuietly(lastLink.toFile());
-
-        try {
-            Files.createSymbolicLink(lastLink, target);
-        } catch (IOException e) {
-            logger.trace("Symbolic link creation error: " + e.getMessage(), e);
-        }
-
-        if (failed) {
-            Path lastFailedLink = Paths.get(lastLogDir.getParent() + File.separator + "lastFailed");
-            deleteLinkQuietly(lastFailedLink.toFile());
-
-            try {
-                Files.createSymbolicLink(lastFailedLink, target);
-            } catch (IOException e) {
-                logger.trace("Symbolic link creation error: " + e.getMessage(), e);
-            }
-        }
-        else {
-            Path lastSuccessfulLink = Paths.get(lastLogDir.getParent() + File.separator + "lastSuccessful");
-            deleteLinkQuietly(lastSuccessfulLink.toFile());
-
-            try {
-                Files.createSymbolicLink(lastSuccessfulLink, target);
-            } catch (IOException e) {
-                logger.trace("Symbolic link creation error: " + e.getMessage(), e);
-            }
-        }
-    }
-
 
     @Override
     public void handle(StartInspector note) {
