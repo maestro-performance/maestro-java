4e963d7cfcbedfea75a2e6318e44701bbbf7d892
Otavio Rodolfo Piske <opiske@redhat.com>
2017-12-13 14:15:14 +0100

    REF: to improve flexibility on the report generator

--- maestro-reports/src/main/java/net/orpiske/mpt/reports/ReportGenerator.java
+++ maestro-reports/src/main/java/net/orpiske/mpt/reports/ReportGenerator.java
@@ -37,8 +37,13 @@ import java.util.Set;
 
 public class ReportGenerator {
     private static final Logger logger = LoggerFactory.getLogger(ReportGenerator.class);
+    private String path;
 
-    private static void plotMptReportFile(ReportFile reportFile) {
+    public ReportGenerator(final String path) {
+        this.path = path;
+    }
+
+    private void plotMptReportFile(final ReportFile reportFile) {
         logger.debug("Will report file {} on thread {}", reportFile, Thread.currentThread().getId());
 
         try {
@@ -62,7 +67,7 @@ public class ReportGenerator {
         }
     }
 
-    private static void plotLatencyReportFile(ReportFile reportFile) {
+    private void plotLatencyReportFile(final ReportFile reportFile) {
         logger.debug("Will report file {} on thread {}", reportFile, Thread.currentThread().getId());
 
         try {
@@ -81,47 +86,7 @@ public class ReportGenerator {
         }
     }
 
-    public static void generate(String path) {
-        File baseDir = new File(path);
-
-        // Step 1: build the file list for processing
-        logger.info("Building the file list to be processed by the report generator");
-        ReportDirProcessor processor = new ReportDirProcessor(path);
-        final List<ReportFile> fileList = processor.generate(baseDir);
-        logger.info("There are {} files to be processed", fileList.size());
-
-        fileList.parallelStream()
-                .filter(item -> item.getSourceFile().getName().endsWith("csv.gz"))
-                .forEach(ReportGenerator::plotMptReportFile);
-
-        // NOTE: this one is not thread-safe. This may have something to do
-        // with capturing the console and so on.
-        // TODO: find a better way to handle this
-        fileList.stream()
-                .filter(item -> item.getSourceFile().getName().endsWith("hdr"))
-                .forEach(ReportGenerator::plotLatencyReportFile);
-
-
-        // Step 2: build the report context
-        Map<String, Object> context = ReportContextBuilder.toContext(fileList, baseDir);
-
-        Set<ReportDirInfo> reports = (Set<ReportDirInfo>) context.get("reportDirs");
-
-        for (ReportDirInfo report : reports) {
-            logger.info("Processing report dir: {}", report.getReportDir());
-            Map<String, Object> nodeReportContext = NodeContextBuilder.toContext(report, baseDir);
-            NodeReportRenderer reportRenderer = new NodeReportRenderer(nodeReportContext);
-
-            try {
-                String outDir = path + report.getReportDir();
-                File outFile = new File(outDir, "index.html");
-                FileUtils.writeStringToFile(outFile, reportRenderer.render(), Charsets.UTF_8);
-                reportRenderer.copyResources(outFile.getParentFile());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
+    private void renderReportIndex(final File baseDir, final Map<String, Object> context) {
         IndexRenderer indexRenderer = new IndexRenderer(context);
         File outFile = new File(path, "index.html");
         try {
@@ -130,8 +95,65 @@ public class ReportGenerator {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    private void renderNodePage(final File baseDir, final ReportDirInfo report) {
+        logger.info("Processing report dir: {}", report.getReportDir());
+        Map<String, Object> nodeReportContext = NodeContextBuilder.toContext(report, baseDir);
+        NodeReportRenderer reportRenderer = new NodeReportRenderer(nodeReportContext);
+
+        try {
+            String outDir = path + report.getReportDir();
+            File outFile = new File(outDir, "index.html");
+            FileUtils.writeStringToFile(outFile, reportRenderer.render(), Charsets.UTF_8);
+            reportRenderer.copyResources(outFile.getParentFile());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generate() {
+        File baseDir = new File(path);
+
+        // Step 1: build the file list for processing
+        logger.info("Building the file list to be processed by the report generator");
+        final ReportDirProcessor processor = new ReportDirProcessor(path);
+        final List<ReportFile> fileList = processor.generate(baseDir);
+        logger.info("There are {} files to be processed", fileList.size());
+
+        fileList.parallelStream()
+                .filter(item -> item.getSourceFile().getName().endsWith("csv.gz"))
+                .forEach(this::plotMptReportFile);
+
+        // NOTE: this one is not thread-safe. This may have something to do
+        // with capturing the console and so on.
+        // TODO: find a better way to handle this
+        fileList.stream()
+                .filter(item -> item.getSourceFile().getName().endsWith("hdr"))
+                .forEach(this::plotLatencyReportFile);
+
+
+        // Step 2: build the report context
+        Map<String, Object> context = ReportContextBuilder.toContext(fileList, baseDir);
+        Set<ReportDirInfo> reports = (Set<ReportDirInfo>) context.get("reportDirs");
+
+        // Step 3: render the pages for each host
+        reports.parallelStream().forEach(item -> renderNodePage(baseDir, item));
+
+        // Step 4: render the index page
+        renderReportIndex(baseDir, context);
+
+        // Step 5: clean the disk
         ReportDirPostProcessor postProcessor = new ReportDirPostProcessor(path);
         postProcessor.postProcess(baseDir);
     }
+
+    @Deprecated
+    public static void generate(final String path) {
+        ReportGenerator reportGenerator = new ReportGenerator(path);
+
+        reportGenerator.generate();
+    }
+
+
 }
