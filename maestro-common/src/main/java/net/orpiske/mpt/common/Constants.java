7b0d571f372ba5adac0301191996f5739035502a
Otavio Rodolfo Piske <opiske@redhat.com>
2018-02-09 10:27:24 +0100

    FIX: updated version string

--- maestro-common/src/main/java/net/orpiske/mpt/common/Constants.java
+++ maestro-common/src/main/java/net/orpiske/mpt/common/Constants.java
@@ -23,7 +23,7 @@ import java.io.File;
  */
 public final class Constants {
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.3.0-SNAPSHOT";
 
 	public static final String BIN_NAME = "maestro-java";
 
