ac342fbddaf5190a4935121eb1d19727a2837f01
Otavio R. Piske <angusyoung@gmail.com>
2018-04-11 20:26:49 +0200

    WIP: dockerfile and supporting code for the inspector container

---
+++ extra/docker/maestro-worker/maestro-inspector/maestro-container-wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Synchronizing time"
+ntpd -d -q -n -p pool.ntp.org
+
+cd /maestro/sender/logs && nohup python -m SimpleHTTPServer 8000 &
+
+${MAESTRO_APP_ROOT}/maestro-inspector/bin/maestro-inspector -m mqtt://maestro-broker:1883 -w org.maestro.worker.jms.JMSSenderWorker -l /maestro/sender/logs -H maestro-sender
