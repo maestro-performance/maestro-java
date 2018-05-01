1f524bc8d74ede8132d52507cca6ac229b2f267d
Otavio Rodolfo Piske <opiske@redhat.com>
2018-05-01 12:33:32 +0200

    UPD: to remove the outdated Python-based data server

--- extra/docker/maestro-worker/maestro-receiver/maestro-container-wrapper.sh
+++ extra/docker/maestro-worker/maestro-receiver/maestro-container-wrapper.sh
@@ -2,6 +2,4 @@
 echo "Synchronizing time"
 ntpd -d -q -n -p pool.ntp.org
 
-cd /maestro/receiver/logs && nohup python -m SimpleHTTPServer 8000 &
-
 ${MAESTRO_APP_ROOT}/maestro-worker/bin/maestro-worker -m mqtt://maestro-broker:1883 -w org.maestro.worker.jms.JMSReceiverWorker -l /maestro/receiver/logs -r receiver -H maestro-receiver
