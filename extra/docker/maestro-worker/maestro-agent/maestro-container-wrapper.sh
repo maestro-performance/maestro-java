5b462e802865f91ea50b23fd3d053a0efb5fc733
Otavio Rodolfo Piske <opiske@redhat.com>
2018-05-01 12:57:45 +0200

    UPD: wrapper scripts to launch maestro peers/workers

---
+++ extra/docker/maestro-worker/maestro-agent/maestro-container-wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Synchronizing time"
+ntpd -d -q -n -p pool.ntp.org
+
+${MAESTRO_APP_ROOT}/maestro-agent/bin/maestro-agent -m mqtt://maestro-broker:1883 -l /maestro/agent/logs
--- extra/docker/maestro-worker/maestro-sender/maestro-container-wrapper.sh
+++ extra/docker/maestro-worker/maestro-sender/maestro-container-wrapper.sh
@@ -2,4 +2,4 @@
 echo "Synchronizing time"
 ntpd -d -q -n -p pool.ntp.org
 
-${MAESTRO_APP_ROOT}/maestro-worker/bin/maestro-worker -m mqtt://maestro-broker:1883 -w org.maestro.worker.jms.JMSSenderWorker -l /maestro/sender/logs -r sender -H maestro-sender
+${MAESTRO_APP_ROOT}/maestro-worker/bin/maestro-worker -m mqtt://maestro-broker:1883 -w org.maestro.worker.jms.JMSSenderWorker -l /maestro/sender/logs -r sender
