e99ffe9e81c6644c56782da461c99939f1b4377f
Otavio Rodolfo Piske <opiske@redhat.com>
2018-05-13 15:44:41 +0200

    FIX: removes launching deprecated data server instance

--- extra/docker-compose/maestro/client/maestro-container-wrapper.sh
+++ extra/docker-compose/maestro/client/maestro-container-wrapper.sh
@@ -3,6 +3,6 @@
 echo "Synchronizing time"
 ntpd -d -q -n -p pool.ntp.org
 mkdir -p /maestro/reports
-cd /maestro/reports && nohup python -m SimpleHTTPServer 8000 &
+cd /maestro/reports
 cd /opt/maestro/maestro-test-scripts
 /bin/bash
