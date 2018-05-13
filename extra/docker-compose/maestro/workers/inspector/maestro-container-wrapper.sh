56159fccbbd1b0284c2dc2a5f18fd37c655c8837
Otavio Rodolfo Piske <opiske@redhat.com>
2018-05-13 16:05:29 +0200

    FIX: removed outdated items from the inspector startup script

--- extra/docker-compose/maestro/workers/inspector/maestro-container-wrapper.sh
+++ extra/docker-compose/maestro/workers/inspector/maestro-container-wrapper.sh
@@ -2,6 +2,4 @@
 echo "Synchronizing time"
 ntpd -d -q -n -p pool.ntp.org
 
-cd /maestro/sender/logs && nohup python -m SimpleHTTPServer 8000 &
-
-${MAESTRO_APP_ROOT}/maestro-inspector/bin/maestro-inspector -m mqtt://broker:1883 -w org.maestro.worker.jms.JMSSenderWorker -l /maestro/sender/logs -H maestro-sender
+${MAESTRO_APP_ROOT}/maestro-inspector/bin/maestro-inspector -m mqtt://broker:1883 -l /maestro/inspector/logs -H inspector
