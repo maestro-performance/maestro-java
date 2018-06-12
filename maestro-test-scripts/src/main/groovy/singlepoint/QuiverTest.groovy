6f56adf0245eaff888a7d0fb561ca8dd1efb7fa9
Otavio Rodolfo Piske <opiske@redhat.com>
2018-06-12 10:01:10 +0200

    UPD: revert the groovy test to not use the builtin runner
    Since it uses external dependencies, the builtin implementation is unstable and causing lots of problems.
    
    Also, allows this to be used as an example of how to implement a fully external test

--- maestro-test-scripts/src/main/groovy/singlepoint/QuiverTest.groovy
+++ maestro-test-scripts/src/main/groovy/singlepoint/QuiverTest.groovy
@@ -17,7 +17,12 @@
 package singlepoint
 
 @GrabConfig(systemClassLoader = true)
+@GrabResolver(name='Eclipse', root='https://repo.eclipse.org/content/repositories/paho-releases/')
+@Grab(group='org.eclipse.paho', module='org.eclipse.paho.client.mqttv3', version='1.1.1')
+
 @GrabResolver(name='orpiske-bintray', root='https://dl.bintray.com/orpiske/libs-release')
+@Grab(group='org.maestro', module='maestro-tests', version='1.3.0-SNAPSHOT')
+@Grab(group='org.maestro', module='maestro-reports', version='1.3.0-SNAPSHOT')
 @Grab(group='net.orpiske', module='quiver-data-plotter', version='1.0.0')
 
 import org.maestro.client.Maestro
