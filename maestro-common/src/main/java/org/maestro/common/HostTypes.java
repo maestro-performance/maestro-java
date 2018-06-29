405175ea616bbb360c92acfc72fba1b782fbe365
Otavio Rodolfo Piske <opiske@redhat.com>
2018-06-29 15:23:09 +0200

    UPD: maestro common host types

---
+++ maestro-common/src/main/java/org/maestro/common/HostTypes.java
@@ -0,0 +1,9 @@
+package org.maestro.common;
+
+public class HostTypes {
+    public static final String SENDER_HOST_TYPE = "sender";
+    public static final String RECEIVER_HOST_TYPE = "receiver";
+    public static final String INSPECTOR_HOST_TYPE = "inspector";
+    public static final String AGENT_HOST_TYPE = "agent";
+    public static final String EXPORTER_HOST_TYPE = "exporter";
+}
