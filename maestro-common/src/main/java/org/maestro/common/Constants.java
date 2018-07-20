7e4ecf0304d228ca739992f770fd8f25c83e2594
Otavio Rodolfo Piske <opiske@redhat.com>
2018-07-20 14:31:14 +0200

    UPD: preparing for 1.3.5 release

--- maestro-common/src/main/java/org/maestro/common/Constants.java
+++ maestro-common/src/main/java/org/maestro/common/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
 
     public static final String TEST_LOGS_CONTEXT = "/logs/tests";
 
-    public static final String VERSION = "1.3.4";
+    public static final String VERSION = "1.3.5";
 
     public static final String BIN_NAME = "maestro-java";
 
