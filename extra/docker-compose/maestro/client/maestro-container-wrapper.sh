97f2f4217bc0574c693dd2ce6568cd92c0da28bc
Otavio Rodolfo Piske <opiske@redhat.com>
2018-08-09 16:37:21 +0200

    UPD: make the time synchronization optional on the containers

--- extra/docker-compose/maestro/client/maestro-container-wrapper.sh
+++ extra/docker-compose/maestro/client/maestro-container-wrapper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Synchronizing time"
-ntpd -d -q -n -p pool.ntp.org
+if [[ ${MAESTRO_TYME_SYNC} == "true" ]] ; then
+    echo "Synchronizing time"
+    ntpd -d -q -n -p pool.ntp.org
+fi
+
 mkdir -p /maestro/reports
 cd /maestro/reports
 cd /opt/maestro/maestro-test-scripts
