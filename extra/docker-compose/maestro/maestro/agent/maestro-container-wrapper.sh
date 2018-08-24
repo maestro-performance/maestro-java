9b769c31dfe918ad9f9db814c0c2f084a4aa35d1
Otavio Rodolfo Piske <opiske@redhat.com>
2018-08-24 16:43:20 +0200

    REF: docker-related files to support the dynamic workers

--- extra/docker-compose/maestro/maestro/agent/maestro-container-wrapper.sh
+++ extra/docker-compose/maestro/maestro/agent/maestro-container-wrapper.sh
@@ -6,6 +6,6 @@ if [[ ${MAESTRO_TYME_SYNC} == "true" ]] ; then
 fi
 
 MAESTRO_BROKER=${MAESTRO_BROKER:-mqtt://broker:1883}
-MAESTRO_LOG_DIR=${MAESTRO_LOG_DIR:-/maestro/receiver/logs}
+MAESTRO_LOG_DIR=${MAESTRO_LOG_DIR:-/maestro/agent/logs}
 
 ${MAESTRO_APP_ROOT}/maestro-agent/bin/maestro-agent -m ${MAESTRO_BROKER} -l ${MAESTRO_LOG_DIR}
