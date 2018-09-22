65fd7c42a0b87f8907f5c2c238a7d6177c249258
Otavio R. Piske <angusyoung@gmail.com>
2018-09-22 11:40:56 +0200

    WIP: initial bits for the reports server

---
+++ maestro-reports/maestro-reports-server/src/main/java/org/maestro/reports/server/DefaultReportsServer.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 Otavio Rodolfo Piske
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.maestro.reports.server;
+
+import io.javalin.Javalin;
+import org.apache.commons.configuration.AbstractConfiguration;
+import org.maestro.common.ConfigurationWrapper;
+
+public class DefaultReportsServer implements ReportsServer {
+
+    public void start() {
+        AbstractConfiguration config = ConfigurationWrapper.getConfig();
+
+        final int port = config.getInteger("maestro.reports.server", 6500);
+
+        Javalin app = Javalin.create()
+                .port(port)
+                .enableStaticFiles("/site")
+                .enableCorsForAllOrigins()
+                .disableStartupBanner()
+                .start();
+
+        app.get("/api/live", ctx -> ctx.result("Hello World"));
+    }
+
+
+}
