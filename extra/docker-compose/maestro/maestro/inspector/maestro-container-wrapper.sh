7f919494851ab58211e19910d0eea026939690c7
Otavio Rodolfo Piske <opiske@redhat.com>
2018-12-03 10:52:39 +0100

    DEL: stop overriding the container hostname for the inspector

--- extra/docker-compose/maestro/maestro/inspector/maestro-container-wrapper.sh
+++ extra/docker-compose/maestro/maestro/inspector/maestro-container-wrapper.sh
@@ -8,4 +8,4 @@ fi
 MAESTRO_BROKER=${MAESTRO_BROKER:-mqtt://broker:1883}
 MAESTRO_LOG_DIR=${MAESTRO_LOG_DIR:-/maestro/inspector/logs}
 
-${MAESTRO_APP_ROOT}/maestro-inspector/bin/maestro-inspector -m ${MAESTRO_BROKER} -l ${MAESTRO_LOG_DIR} -H inspector
+${MAESTRO_APP_ROOT}/maestro-inspector/bin/maestro-inspector -m ${MAESTRO_BROKER} -l ${MAESTRO_LOG_DIR}
