c1745f20ea4af97298034c555b192f5b0ccc2229
Otavio R. Piske <angusyoung@gmail.com>
2018-12-07 16:11:03 +0100

    FIX: filter out the anchor part of the URL when parsing the parameters

--- maestro-reports/maestro-reports-server/src/main/resources/site/maestro/js/common/maestro.url.utils.js
+++ maestro-reports/maestro-reports-server/src/main/resources/site/maestro/js/common/maestro.url.utils.js
@@ -1,6 +1,6 @@
 function getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&#]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
     return vars;
