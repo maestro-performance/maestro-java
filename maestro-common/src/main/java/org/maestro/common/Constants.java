5f6a6d78cadd85ca75b2006d3117bea38d5fcf54
Otavio Rodolfo Piske <opiske@redhat.com>
2018-12-17 09:19:15 +0100

    UPD: prepare for Maestro 1.5.2 release
    - update versions
    - update tags used by images files

--- maestro-common/src/main/java/org/maestro/common/Constants.java
+++ maestro-common/src/main/java/org/maestro/common/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
 
     public static final String TEST_LOGS_CONTEXT = "/logs/tests";
 
-    public static final String VERSION = "1.5.2-SNAPSHOT";
+    public static final String VERSION = "1.5.2";
 
     public static final int VERSION_NUMERIC;
 
