35440e39205e63abfb454059440ede879372901e
Otavio R. Piske <angusyoung@gmail.com>
2019-07-29 14:08:41 +0200

    Revert "Bumped lots of versions for 1.5.6-SNAPSHOT"
    This reverts commit a13947084df5eb2642be14a600514a0e69864e0d.

--- maestro-common/src/main/java/org/maestro/common/Constants.java
+++ maestro-common/src/main/java/org/maestro/common/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
 
     public static final String TEST_LOGS_CONTEXT = "/logs/tests";
 
-    public static final String VERSION = "1.5.6-SNAPSHOT";
+    public static final String VERSION = "1.5.5";
 
     public static final int VERSION_NUMERIC;
 
